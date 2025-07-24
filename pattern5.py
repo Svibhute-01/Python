373add3d852239752c2123059a9c1fd801870f9a
Svibhute-01 <vibhutesnehal727@gmail.com>
2025-07-24 13:38:50 +0530

    this is 1st commit to check

--- pattern5.py
+++ pattern5.py
@@ -1,7 +1,5 @@
-n=int(input("Enter value of n "))
-x=65+n+1
-for i in range(65,x):
-    for j in range (65,i):
-        print ((chr(j)),end=' ')
-    print("\n")    
-             
+n = int(input("Enter value of n "))
+for i in range(n, 0, -1):            
+    for j in range(65, 65 + i):      
+        print(chr(j), end=' ')
+    print()
