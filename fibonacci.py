c90cee5f5a9a014276e09e880d704e28bc9e174d
Svibhute-01 <snehalv727@gmail.com>
2025-08-04 14:29:32 +0530

    new code

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,15 @@
 n=int(input("Enter value of n"))
 t1=0
 t2=1
-while i<n:
+for i in range(0,n):
+  if(i==0):
+    sum=0
+  elif(i==1):
+    sum=1
+  else:
+    sum=t1+t2
+    t1=t2
+    t2=sum
+  print(sum,end=" ")
+  
+    
