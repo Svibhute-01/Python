59a8b1ad045f64c710ce538ce7bcaa636e4b41ab
Svibhute-01 <snehalv727@gmail.com>
2025-08-05 17:05:33 +0530

    Modified Code

--- largestNum.py
+++ largestNum.py
@@ -1,4 +1,14 @@
-n=int(input())
+n=int(input("How many numbers are there in lis?"))
 print("Enter",n,"numbers:")
+l=[]
 for i in range (n):
-    int(input())
+  x=int(input())
+  l.append(x)
+
+largest=l[0]
+for i in l:
+  if(i>=largest):
+    largest=i
+  else:
+    largest=largest
+print("Largest Number is:" ,largest)
